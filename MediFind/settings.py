a799b59d6a7e0471a54f979c50a8296647b9582e
Bhaumik-Mhatre <mhatrebhaumik@gmail.com>
2025-03-08 03:15:57 +0530

    new file:   .env 	modified:   MediFind/__pycache__/settings.cpython-312.pyc 	modified:   MediFind/__pycache__/urls.cpython-312.pyc 	new file:   MediFind/__pycache__/wsgi.cpython-312.pyc 	modified:   MediFind/settings.py 	modified:   MediFind/urls.py 	new file:   core/__pycache__/__init__.cpython-312.pyc 	new file:   core/__pycache__/urls.cpython-312.pyc 	new file:   core/__pycache__/views.cpython-312.pyc 	modified:   core/urls.py 	modified:   core/views.py 	renamed:    static/scripts/scripts.js -> static/scripts.js 	renamed:    static/stylesheet/style.css -> static/styles.css 	modified:   templates/index.html 	new file:   venv/Lib/site-packages/psycopg2-2.9.10.dist-info/INSTALLER 	new file:   venv/Lib/site-packages/psycopg2-2.9.10.dist-info/LICENSE 	new file:   venv/Lib/site-packages/psycopg2-2.9.10.dist-info/METADATA 	new file:   venv/Lib/site-packages/psycopg2-2.9.10.dist-info/RECORD 	new file:   venv/Lib/site-packages/psycopg2-2.9.10.dist-info/REQUESTED 	new file:   venv/Lib/site-packages/psycopg2-2.9.10.dist-info/WHEEL 	new file:   venv/Lib/site-packages/psycopg2-2.9.10.dist-info/top_level.txt 	new file:   venv/Lib/site-packages/psycopg2/__init__.py 	new file:   venv/Lib/site-packages/psycopg2/__pycache__/__init__.cpython-312.pyc 	new file:   venv/Lib/site-packages/psycopg2/__pycache__/_ipaddress.cpython-312.pyc 	new file:   venv/Lib/site-packages/psycopg2/__pycache__/_json.cpython-312.pyc 	new file:   venv/Lib/site-packages/psycopg2/__pycache__/_range.cpython-312.pyc 	new file:   venv/Lib/site-packages/psycopg2/__pycache__/errorcodes.cpython-312.pyc 	new file:   venv/Lib/site-packages/psycopg2/__pycache__/errors.cpython-312.pyc 	new file:   venv/Lib/site-packages/psycopg2/__pycache__/extensions.cpython-312.pyc 	new file:   venv/Lib/site-packages/psycopg2/__pycache__/extras.cpython-312.pyc 	new file:   venv/Lib/site-packages/psycopg2/__pycache__/pool.cpython-312.pyc 	new file:   venv/Lib/site-packages/psycopg2/__pycache__/sql.cpython-312.pyc 	new file:   venv/Lib/site-packages/psycopg2/__pycache__/tz.cpython-312.pyc 	new file:   venv/Lib/site-packages/psycopg2/_ipaddress.py 	new file:   venv/Lib/site-packages/psycopg2/_json.py 	new file:   venv/Lib/site-packages/psycopg2/_psycopg.cp312-win_amd64.pyd 	new file:   venv/Lib/site-packages/psycopg2/_range.py 	new file:   venv/Lib/site-packages/psycopg2/errorcodes.py 	new file:   venv/Lib/site-packages/psycopg2/errors.py 	new file:   venv/Lib/site-packages/psycopg2/extensions.py 	new file:   venv/Lib/site-packages/psycopg2/extras.py 	new file:   venv/Lib/site-packages/psycopg2/pool.py 	new file:   venv/Lib/site-packages/psycopg2/sql.py 	new file:   venv/Lib/site-packages/psycopg2/tz.py

--- MediFind/settings.py
+++ MediFind/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +76,13 @@ WSGI_APPLICATION = 'MediFind.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'DBNAME' : 'postgres',
+        'USER' : 'postgres.qelvcbcwlwzaqlcrcdrw', 
+        'PASSWORD' : 'Bhaumik13122004@',
+        'HOST' : 'aws-0-ap-south-1.pooler.supabase.com',
+        'PORT' : '5432',
+        
     }
 }
 
@@ -116,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
